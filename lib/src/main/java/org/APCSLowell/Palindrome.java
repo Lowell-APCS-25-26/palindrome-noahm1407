7a0c554203ef2ba7f744a91f0912c14cacd1cf19
noahm1407 <noahmuchowski@gmail.com>
2026-01-29 17:55:36 +0000

    All tests successful

--- lib/src/main/java/org/APCSLowell/Palindrome.java
+++ lib/src/main/java/org/APCSLowell/Palindrome.java
@@ -2,11 +2,36 @@ package org.APCSLowell;
 
 public class Palindrome {
     public boolean palindrome(String s) {
-        return false;
+    s = s.toLowerCase();
+    s = noSpaces(s);
+    s = onlyLetters(s);
+    return s.equals(reverse(s));
     }
+
     public String reverse(String s) {
-        String n = new String();
-        // your code here
-        return n;
+    String n = "";
+    for (int i = s.length()- 1; i >= 0; i--)
+    n += s.charAt(i);
+    return n;
     }
+public String noSpaces(String sWord){
+String s = "";
+for (int i = 0; i < sWord.length(); i++)
+if (sWord.charAt(i) != ' ')
+s = s + sWord.charAt(i);
+
+return s;
 }
+
+public String onlyLetters(String sString){
+  String s = "";
+  for (int i = 0; i < sString.length(); i++)
+  if (Character.isLetter(sString.charAt(i)) == true)
+  s = s + sString.charAt(i);
+  
+  return s;
+}
+
+
+}
+
